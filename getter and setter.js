26d4ea94228b22cd24daf399fd91dc09859690f6
Sanvi Rahman <SanviRahman>
2025-08-04 15:42:19 +0600

    Completed task getter  and setter

---
+++ getter and setter.js
@@ -0,0 +1,28 @@
+var Man = /** @class */ (function () {
+    function Man(age, name) {
+        this.age = age;
+        this.name = name;
+    }
+    Man.prototype.getAge = function () {
+        return this.age;
+    };
+    Man.prototype.setAge = function (age) {
+        this.age = age;
+    };
+    Man.prototype.getName = function () {
+        return this.name;
+    };
+    Man.prototype.setName = function (name) {
+        this.name = name;
+    };
+    return Man;
+}());
+var person = new Man(25, "John Doe");
+//name
+console.log("Name: ".concat(person.getName()));
+person.setName("Jane Doe");
+console.log("Updated Name: ".concat(person.getName()));
+//age
+console.log("Age: ".concat(person.getAge()));
+person.setAge(30);
+console.log("Updated Age: ".concat(person.getAge()));
---
+++ getter and setter.ts
@@ -0,0 +1,42 @@
+class Man{
+    private age: number;
+    private name: string;
+
+
+    constructor(age: number, name: string) {
+        this.age = age;
+        this.name = name;
+    }
+
+
+    getAge(): number {
+        return this.age;
+    }
+
+    setAge(age: number): void {
+        this.age = age;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    setName(name: string): void {
+        this.name = name;
+    }
+}
+
+
+
+let person = new Man(25, "John Doe");
+
+
+//name
+console.log(`Name: ${person.getName()}`);
+person.setName("Jane Doe");
+console.log(`Updated Name: ${person.getName()}`);
+
+//age
+console.log(`Age: ${person.getAge()}`);
+person.setAge(30);
+console.log(`Updated Age: ${person.getAge()}`);
